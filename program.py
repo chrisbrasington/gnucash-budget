e8a47b6edb721098ba99fbb971df904253e2316d
chrisbrasington <chrisbrasington@gmail.com>
2017-01-03 21:23:35 -0700

    budget program

--- program.py
+++ program.py
@@ -1,19 +1,53 @@
 #!/usr/bin/python2.7
-import os, piecash, yaml, time
+import os, piecash, yaml, time, math
 from piecash import open_book, Transaction, Split, Account
-#from piecash.core.factories import create_currency_from_ISO
 from pprint import pprint
 
 class account:
-	name = ""
 	amount = 0
 	max = 0
+	name = ""
+
+	def percentage(self):
+		if self.amount == 0 or self.max == 0:
+			return "0%"
+		return "{0:.0f}%".format((self.amount/self.max)*100)
 
 	def __init__(self, name):
 		self.name = name
 
+	def __init__(self, name, max):
+		self.name = name
+		self.max = max
+
 	def __str__(self):
-		s = self.name + ' ' + str(self.amount)
+		s = self.name
+		if(len(s) <=3):
+			s += ' \t'
+		if(len(s) <=12):
+			s += ' \t'
+
+		s += str(self.amount) + ' '
+		s += '\t'
+
+		percent = int(self.percentage().rstrip("%"))
+
+		if(self.amount < 1000):
+			s += '\t'
+		s += '['
+
+		i = 0
+
+		range_max = int(math.ceil(percent / 10.0)) 
+
+		for x in range(0, range_max):
+			s += '-'
+			i += 1
+		for x in range(i, 10):
+			s += ' '
+		s += ']'
+
+		s += str(self.percentage())
 		return s
 
 class budget:
@@ -22,12 +56,25 @@ class budget:
 	name = ""
 	accounts = []
 
+	def percentage(self):
+		if self.amount == 0 or self.max == 0:
+			return "0%"
+		return "{0:.0f}%".format((self.amount/self.max)*100)
+
 	def __init__(self, name):
 		self.name = name
 		self.accounts = []
 
+	def __init__(self, name, max):
+		self.name = name
+		self.max = max
+		self.accounts = []
+
 	def __str__(self):
-		s = self.name + ' '+ str(self.amount) + "\n"
+		s = self.name + ' '+ str(self.amount) 
+		s += " / " + str(self.max)
+		s += "\n"
+		s += str(self.percentage()) + "\n\n"
 		for a in self.accounts:
 			s += '   ' + str(a) + "\n"
 		return s
@@ -67,26 +114,6 @@ def checkBalance(book, account, year, month, budget_essentials, budget_savings, 
 					found = True
 					print '\tSavings', 
 					break
-
-		# for a in budget_file['essentials']['accounts'].split(','):
-		# 	if a.strip() in account.fullname:
-		# 		budget_file['essentials']['value'] += sp.value
-		# 		print '\tEssential', 
-		# 		found = True
-		# 		break
-
-		# if not found:
-		# 	for a in budget_file['savings']['accounts'].split(','):
-		# 		if a.strip() in account.name:
-		# 			budget_file['savings']['value'] += sp.value
-		# 			print '\tSavings', 
-		# 			found = False
-		# 			break
-
-		# if not found:
-		# 	budget_file['personal']['value'] += sp.value
-		# 	print '\tPersonal', 
-		# 	found = True
 		
 		if not found:
 			budget_personal.amount += sp.value
@@ -106,17 +133,20 @@ with open(settings_file) as ymlfile:
 book_path = budget_file['file']
 
 
-budget_essentials = budget(name = "Essentials")
-budget_savings = budget(name = "Savings")
-budget_personal = budget(name = "Personal")
+budget_essentials = budget(name = "Essentials", max = budget_file['essentials']['budget'])
 
-for a in budget_file['essentials']['accounts'].split(','):
-	a = a.strip()
-	budget_essentials.accounts.append(account(name=a))
+budget_savings = budget(name = "Savings", max = budget_file['savings']['budget'])
 
-for a in budget_file['savings']['accounts'].split(','):
-	a = a.strip()
-	budget_savings.accounts.append(account(name=a))
+budget_personal = budget(name = "Personal", max = budget_file['personal']['budget'])
+
+budget_income = budget(name = "Income", max = budget_file['income']['budget'])
+
+for a in budget_file['essentials']['accounts']:
+	budget_essentials.accounts.append(account(name=a['name'], max = a['budget']))
+
+
+for a in budget_file['savings']['accounts']:
+	budget_savings.accounts.append(account(name=a['name'], max = a['budget']))
 
  # check for existance of to_account and from_account
 book = piecash.open_book(book_path)
@@ -126,26 +156,19 @@ month = time.strftime('%b')
 
 print month, ' ', year
 print
+print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 
 for a in book.accounts:
 	if a.fullname.startswith("Expenses"):
 		budget_file = checkBalance(book, a.fullname, year, month, budget_essentials, budget_savings, budget_personal)
 
+print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+
 print 
 print budget_essentials
 
-print budget_savings
-
 print budget_personal
 
+print budget_income
 
-
-# print 'savings:'
-# print budget_file['savings']
-# print 'personal:'
-# print budget_file['personal']
-
-# a = "Groceries"
-# b = "Expenses:Groceries:H-Mart"
-
-# print a in b
+print budget_savings
